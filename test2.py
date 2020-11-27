76585ad519565ac36504a834daf748f45cf9b698
weihaodong <weihaodong@4paradigm.com>
2020-11-27 18:37:39 +0800

    增加传递大参数与返回大数据量耗时统计

--- test2.py
+++ test2.py
@@ -1,6 +1,28 @@
 import jpype
 import time
 
+def buildComplexObject():
+    complexObject = ComplexObject()
+    complexObject.setIntValue(Integer(1))
+    complexObject.setLongValue(1)
+    complexObject.setDoubleValue(1.0)
+    complexObject.setStr("hello 1")
+    listInParam = ArrayList()
+    listInParam.add(1)
+    listInParam.add(3)
+    listInParam.add(5)
+    complexObject.setList(listInParam)
+    map = HashMap()
+    map.put("int", Integer(1))
+    map.put("double", 1)
+    map.put("str", "hello 1")
+    complexObject.setMap(map)
+    data = Data()
+    data.setData("hello data 1")
+    complexObject.setData(data)
+    return complexObject
+
+
 if __name__ == '__main__':
     jvmArg = '-Djava.class.path=/Users/4paradigm/IdeaProjects/Jpype-test/out/artifacts/Jpype_test_jar/Jpype-test.jar'
     print(jpype.getDefaultJVMPath())
@@ -122,5 +144,116 @@ if __name__ == '__main__':
     resultData = resultObject.getData()
     print(resultData.getData())
     print('--- --- ---')
+
+    '''数据结构测试 map2'''
+    param = HashMap()
+    list = ArrayList()
+    list.add(1)
+    list.add(3)
+    list.add(5)
+    map = HashMap()
+    map.put("int", Integer(1))
+    map.put("double", 1)
+    map.put("str", "hello 1")
+    object = buildComplexObject()
+    param.put("map", map)
+    param.put("list", list)
+    param.put("object", complexObject)
+    result = test2.testMap2(param)
+    print(result)
+    print("list start")
+    for i in result.get("list"):
+        print(i)
+    print("list end")
+    print("map start")
+    for entry in result.get("map").entrySet():
+        print(entry.getKey() + " : " + str(entry.getValue()))
+    print("map end")
+    print(result.get("object"))
+    print('--- --- ---')
+
+    '''数据结构测试 list2'''
+    param = ArrayList()
+    list = ArrayList()
+    list.add(1)
+    list.add(3)
+    list.add(5)
+    map = HashMap()
+    map.put("int", Integer(1))
+    map.put("double", 1)
+    map.put("str", "hello 1")
+    object = buildComplexObject()
+    param.add(map)
+    param.add(list)
+    param.add(object)
+    result = test2.testList2(param)
+    print(result)
+    print("map start")
+    for entry in result.get(0).entrySet():
+        print(entry.getKey() + " : " + str(entry.getValue()))
+    print("map end")
+    print("list start")
+    for i in result.get(1):
+        print(i)
+    print("list end")
+    print(result.get(2))
+    print('--- --- ---')
+
+    '''测量10000个key value的简单数值map传递耗时'''
+    param = HashMap()
+    num = 1
+    for i in range(10000):
+        param.put(num, num)
+        num = num + 2
+    start = time.time()
+    result = test2.testMap3(param)
+    end = time.time()
+    print(end - start)
+    print((end - start) * 1000)
+    print(result.size())
+    print('--- --- ---')
+
+    '''测量10000个element的简单数值list传递耗时'''
+    param = ArrayList()
+    num = 1
+    for i in range(10000):
+        param.add(num)
+        num = num + 2
+    start = time.time()
+    result = test2.testList3(param)
+    end = time.time()
+    print(end - start)
+    print((end - start) * 1000)
+    print(result.size())
+    print('--- --- ---')
+
+    '''测量100个复杂对象map传递耗时'''
+    param = HashMap()
+    num = 1
+    for i in range(100):
+        object = buildComplexObject()
+        param.put(num, object)
+        num = num + 2
+    start = time.time()
+    result = test2.testMap4(param)
+    end = time.time()
+    print(end - start)
+    print((end - start) * 1000)
+    print(result.size())
+    print('--- --- ---')
+
+    '''测量100个复杂对象list传递耗时'''
+    param = ArrayList()
+    for i in range(100):
+        object = buildComplexObject()
+        param.add(object)
+    start = time.time()
+    result = test2.testList4(param)
+    end = time.time()
+    print(end - start)
+    print((end - start) * 1000)
+    print(result.size())
+    print('--- --- ---')
+
 
     jpype.shutdownJVM()
